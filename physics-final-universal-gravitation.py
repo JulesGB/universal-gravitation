613b610babc789673fed1ac2b8db44def76d7c4f
JulesGB <jgoduco.bun@gmail.com>
2020-06-01 19:16:34 -0500

    Added demos, allowed for variable scale

--- physics-final-universal-gravitation.py
+++ physics-final-universal-gravitation.py
@@ -12,10 +12,12 @@ G = 6.67408 * (10 ** -11)
 
 # Astronomical Unit (meters)
 AU = 1.495978707 * (10 ** 11)
+SCALE = 300 / AU
 
 
 class Mass:
-	def __init__(self, m, x, y, vx, vy, r):
+	def __init__(self, name, m, x, y, vx, vy, r):
+		self.name = name
 		self.mass = m
 		self.x = x
 		self.y = y
@@ -28,6 +30,10 @@ class Mass:
 		dy = self.y - other.y
 		d = math.sqrt(dx**2 + dy**2)
 
+		# Check for collisions
+		if d == 0:
+			raise Exception('Collision between {} and {},'.format(self.name, other.name))
+
 		f = G * self.mass * other.mass / (d ** 2)  # Universal Force of Gravity: F = (G * m1 * m2) / (r^2)
 		theta = math.atan2(dy, dx)  # Calculate angle of force to get component vectors
 		fx = f * math.cos(theta)
@@ -37,7 +43,7 @@ class Mass:
 
 
 class Simulation:
-	def __init__(self, spf, masses, size=(1280,720), fps=60):
+	def __init__(self, spf, masses, size=(1280,720), fps=60, scale=SCALE):
 		self.spf    = spf   # seconds per frame
 		self.masses = masses
 		self.size   = size  # window size
@@ -86,15 +92,51 @@ class Simulation:
 				mass.x += mass.vx * self.spf
 				mass.y += mass.vy * self.spf
 
-				# Draw mass on screen
-				pygame.draw.circle(screen, white, (int(mass.x), int(mass.y)), mass.radius)
+				# Draw mass on screen, adjust origin to center of screen
+				# Radius not drawn to scale for visibility
+				pygame.draw.circle(screen, white, (int(mass.x * scale + 640), int(mass.y * scale + 360)), int(mass.radius))
 
 			pygame.display.flip()
 			clock.tick(self.fps)
 
-# TO-DO
-# 1. Add scale
-# 2. Gather and input actual planetary data
-# 3. Add trails
+# Demos
 
-Simulation(3600 * 24, [Mass(1000, 400, 400, 0, 0, 50), Mass(300, 400, 175, 0.00001, 0.00000001, 10), Mass(30, 550, 600, 0, -0.00002, 5)])
+# Orbit via calculation
+# When one mass is significantly larger than another:
+# v0 = sqrt(G * M / r)
+# v0 is orbit velocity
+# M is the mass of the object being orbitted
+# r is orbit radius
+
+# m1 = 10,000 kg
+# m2 = 25 kg
+# r = 50 m
+
+# v0 = sqrt(G * (10000kg) / (50 m)) = 1.155 * 10^-4 m/s
+
+Simulation(3600,
+	[Mass('1', 10**7, 0, 0, 0, 0, 50),
+	Mass('2', 100, 100, 0, 0, math.sqrt(G * 10**7 / 100), 10)],
+	scale=1)
+
+# Planetary Data gathered from https://nssdc.gsfc.nasa.gov/planetary/planetfact.html
+# Given only mass, distance from sun (semimajor axis), and avg velocity
+earth = Mass('Earth', 5.9724 * 10**24, -1*AU, 0, 0, 29.78 * 1000, 5)
+sun = Mass('Sun', 1988500 * 10**24, 0, 0, 0, 0, 15)
+mercury = Mass('Mercury', 0.3301 * 10**24, -0.3871*AU, 0, 0, 47360, 3)
+venus = Mass('Venus', 4.8675 * 10**24, -0.7233*AU, 0, 0, 35020, 3)
+
+Simulation(3600 * 24, [earth, sun, mercury, venus])
+
+Simulation(3600 * 24,
+	[Mass('1', 500, 100, 0, 0, 0, 5),
+	Mass('2', 400, -100, 0, 0, 0, 2),
+	Mass('3', 700, 0, 100, 0, -0.000001, 7)],
+	scale=1)
+
+Simulation(3600 * 12,
+	[Mass('1', 5, 0, 10, 0.00001, 0, 5),
+	Mass('2', 10, 3, 5, 0, -0.00001, 10),
+	Mass('3', 7, 4, 0, 0.00001, 0, 7),
+	Mass('4', 3, -5, -2, 0, 0, 3)],
+	scale=5)
